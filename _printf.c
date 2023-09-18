d2ee9f9f2f396a64d4ee60335f3b112dcaa8d843
eyzee <azizihamza1910@gmail.com>
2023-09-18 03:36:06 +0100

    !

--- _printf.c
+++ _printf.c
@@ -27,14 +27,6 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-
-			if (format[i + 1] == 32)
-			{
-				for (i = i + 1; format[i + 1] == 32; i++)
-					check = 1;
-			}
-			if (check == 1 && (format[i + 1] == 'd' || format[i + 1] == 'i'))
-				write(1, " ", 1);
 			if (!spy_cmp(format[i + 1]) && format[i + 1] != 32)
 			{
 				count += percent_hand();
